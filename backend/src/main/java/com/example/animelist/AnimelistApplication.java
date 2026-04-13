9870d9aad030693a9cab54ec42ba2dc99c71a67e
TueMinh-Sunna <23630701.minh@student.iuh.edu.vn>
2026-04-13 16:18:54 +0700

    track backend files

---
+++ backend/src/main/java/com/example/animelist/AnimelistApplication.java
@@ -0,0 +1,11 @@
+package com.example.animelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimelistApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AnimelistApplication.class, args);
+    }
+}
