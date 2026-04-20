19df6a6178c4f5add4a633cf9f4dc4d77542da79
QTru <23735771.tru@student.iuh.edu.vn>
2026-04-20 09:47:32 +0700

    refactor frontend: theme tokens

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
 import App from "./App";
+import "./styles/Theme.module.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
